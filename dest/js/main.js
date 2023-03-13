29e2e631f28973e590ce529b2380f615ba89c98a
kenhua-l <l_kenhua93@hotmail.com>
2023-03-14 01:08:45 +0800

    add project details to the modal

--- dest/js/main.js
+++ dest/js/main.js
@@ -39,10 +39,42 @@ jQuery.fn.ListProjects = function(options) {
   })
 }
 
+function projectOpen($thiselem, title, imgPath) {
+  let project = dataJson.find(function(elem) {
+    return elem.title === title;
+  })
+  // change title
+  $thiselem.find('.modal-title').html(project.title);
+  // change status
+  $thiselem.find('.modal-status').html('Status: ' + project.status);
+  // change link
+  if(project.link) {
+    $thiselem.find('.modal-link').html('Link: <a href="' + project.link + '" target="_blank">' + project.link + '</a>');
+  } else {
+    $thiselem.find('.modal-link').html('');
+  }
 
+  // add description in info
+  if(project.details && Array.isArray(project.details)) {
+    let modalInfo = $thiselem.find('.modal-info').empty();
+    let modalRow = $('<div class="row"></div>');
+    let modal6img = $('<div class="col-6"></div>');
+    modal6img.append('<img class="project-img" src="' + imgPath + project.image + '.png" alt="' + project.title + '" />');
+    modalRow.append(modal6img);
+    let modal6info = $('<div class="col-6"></div>');
+    modal6info.append('<div class="project-detail">' + project.details.join('') + '</div>');
+    modalRow.append(modal6info);
+    modalInfo.append(modalRow);
+  } else if(project.details) {
+    $thiselem.find('.modal-info').empty();
+    $thiselem.find('.modal-info').append('<div class="project-detail">' + project.details + '</div>');
+    $thiselem.find('.modal-info').append('<img class="project-img" src="' + imgPath + project.image + '.png" alt="' + project.title + '" />');
+  } else {
+    $thiselem.find('.modal-info').empty();
+    $thiselem.find('.modal-info').append('<img class="project-img" src="' + imgPath + project.image + '.png" alt="' + project.title + '" />');
+  }
+  // $thiselem.find('.project-img').attr('src', imgPath + project.image + '.png');
 
-
-function projectOpen(index) {
 
 }
 
@@ -51,6 +83,12 @@ $(function() {
   $('.project-list-container').ListProjects({
     data: '/data/projects.json',
     imgPath: '/img/'
+  });
+
+  // open the project modal
+  $('#project').on('show.bs.modal', function(e) {
+    let project = $(e.relatedTarget).data('project');
+    projectOpen($(this), project, '/img/');
   });
 
   $('.project-modal > .btn-prev').on('click', function() {
